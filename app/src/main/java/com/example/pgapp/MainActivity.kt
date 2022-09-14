5109e0af9820110792bc8b5e7b921bc8583edbd5
guigomo17 <guigomo17@hotmail.com>
2022-09-13 22:50:42 -0400

    Commit

--- app/src/main/java/com/example/pgapp/MainActivity.kt
+++ app/src/main/java/com/example/pgapp/MainActivity.kt
@@ -2,10 +2,73 @@ package com.example.pgapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var handler: Handler
+    private lateinit var imageList: ArrayList<Int>
+    private lateinit var adapter: imagereAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        setUpTransformer()
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable, 2000)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
+    }
+
+    private fun setUpTransformer() {
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer{ page, position ->
+            val  r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init (){
+        viewPager2 = findViewById(R.id.vpMenuCarrusel)
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.descuentoproductos)
+        imageList.add(R.drawable.beneficiosagroalpha)
+        imageList.add(R.drawable.beneficioskadabra)
+        imageList.add(R.drawable.beneficiosviovan)
+
+        adapter = imagereAdapter(imageList, viewPager2)
+
+        viewPager2.adapter = adapter
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
     }
 }
